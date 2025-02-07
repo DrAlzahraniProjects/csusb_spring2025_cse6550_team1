4edb255ae9a594d202f3dc43d59e025150bfd077
Utsav Dharani <UtsavDharani31>
2025-02-06 21:37:33 -0800

    initial commit

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!' #Added Hello World base comment 
+    project_title = 'Team 1: CSUSB Study Podcast Assistant' # Project Title  
+    return f'{project_title}\nHello World!' #Added Hello World base comment 
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 2500))
