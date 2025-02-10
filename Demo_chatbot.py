76bc933d9e038956977a85850af67b4840355800
Utsavdharani31 <UtsavDharani31>
2025-02-09 16:44:57 -0800

    Initial commit

---
+++ Demo_chatbot.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import random,time
+from huggingface_hub import InferenceClient
+
+
+
+def response_generator(prompt):
+    client = InferenceClient(api_key="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")
+    
+    messages = [{"role": "user", "content": prompt}]
+    
+    stream = client.chat.completions.create(
+        model="microsoft/Phi-3.5-mini-instruct",  # Replace with your desired model
+        messages=messages,
+        max_tokens=500,
+        stream=True
+    )
+    
+    response = "".join(chunk.choices[0].delta.content for chunk in stream)
+    return response
+
+
+# Streamlit app setup
+st.title("Chatbot Assistant")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    response = response_generator(prompt)
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
